2a19a36401ff5a3a5e2d7d95d3dc914f0e4f737c
tunglam612 <118800386+tunglam612@users.noreply.github.com>
2024-12-07 01:03:39 +0700

    Update config.h

--- keyboards/sofle/rev1/keymaps/vial/config.h
+++ keyboards/sofle/rev1/keymaps/vial/config.h
@@ -27,6 +27,17 @@
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 5, 3, 8 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
 
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      4
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
 /*
         #ifdef RGB_MATRIX_ENABLE
         #    define RGB_MATRIX_LED_COUNT 72
